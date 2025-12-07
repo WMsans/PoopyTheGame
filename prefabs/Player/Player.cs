84f6b571a9875e2856888b3dadebe1be77dd7603
devdral <84059355+devdral@users.noreply.github.com>
2025-12-07 15:48:12 -0800

    Raycast physics (very wip)

--- prefabs/Player/Player.cs
+++ prefabs/Player/Player.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace PoopyTheGame.Player;
 
-public partial class Player : CharacterBody2D
+public partial class Player : Node2D
 {
     private static readonly float QuarterRotation = float.DegreesToRadians(90);
     
@@ -14,34 +14,48 @@ public partial class Player : CharacterBody2D
     [Export(PropertyHint.Range, "0,1")] public double FrictionPercent { get; set; } = 0.2;
 
     private double _speed;
-    
+    private RayCast2D _rayCast;
+
+    public override void _Ready()
+    {
+        _rayCast = GetNode<RayCast2D>("RayCast2D");
+    }
+
     public override void _PhysicsProcess(double delta)
     {
         if (Input.IsActionPressed("left"))
         {
-            _speed -= Acceleration * delta;
+            _speed -= Acceleration;
         }
         else if (Input.IsActionPressed("right"))
         {
-            _speed += Acceleration * delta;
+            _speed += Acceleration;
         }
         else
         {
             if (_speed > 0)
             {
-                _speed -= Deceleration * delta;
+                _speed -= Deceleration;
             }
             else if (_speed < 0)
             {
-                _speed += Deceleration * delta;
+                _speed += Deceleration;
             }
         }
-
+        
+        _rayCast.ForceRaycastUpdate();
+        var obj = _rayCast.GetCollider();
         var baseVector = Vector2.Right * (float)_speed;
-        Velocity = baseVector with { Y = (float)(Gravity * delta) };
-        MoveAndSlide();
-        var collision = GetLastSlideCollision();
-        if (collision is not null)
-            Rotation = -collision.GetAngle();
+        if (obj is StaticBody2D body)
+        {
+            baseVector = baseVector.Rotated(body.Rotation);
+            Rotation = body.Rotation;
+        }
+        else
+        {
+            Rotation = 0;
+            baseVector += Vector2.Down * (float)Gravity;
+        }
+        Position += baseVector * (float)delta;
     }
 }
